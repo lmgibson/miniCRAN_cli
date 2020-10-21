727589c21c4b82d4e29446ca0e5e6a44ca97b380
lmgibson <landon.g42@gmail.com>
2020-10-21 08:21:14 -0700

    Combined the creation functionality into the addPackageToRepo script. Added flag for whether or not the repo needs to be created first.

--- addPackageToRepo.R
+++ addPackageToRepo.R
@@ -20,9 +20,13 @@ library("miniCRAN")
 library("argparse")
 
 p <- ArgumentParser(description = "Add package to miniCRAN repo")
-p$add_argument("--pkgs", nargs="+" , help = "Name of packages to add to the repo")
+p$add_argument("--pkgs", nargs="+" , help = "Names of packages to add to the repo")
 p$add_argument("--repo", help = "Location of your miniCRAN repo")
+p$add_argument("--create", default=FALSE, help = "A flag for whether or not the repo needs to be created first. Defaults to 'FALSE'.")
+p$add_argument("--rversion", default = "3.5.1", help = "Your R version. Defaults to 3.5.1.")
 args <- p$parse_args(commandArgs(trailingOnly=TRUE))
+CRAN_mirror <- c(CRAN = 'https://cloud.r-project.org/')
+
 
 checkArgs = function(args_list) {
     if (length(args_list$pkgs) == 0) {
@@ -32,18 +36,35 @@ checkArgs = function(args_list) {
     }
 }
 
+
+createRepo <- function(args_list) {
+    print(paste0("Creating repo at: ", args_list$repo))
+    pkgs_expanded <- pkgDep(args_list$pkgs, repos = CRAN_mirror)
+    makeRepo(pkgs_expanded,
+        path = args_list$repo,
+        repos = CRAN_mirror,
+        type = "win.binary",
+        Rversion = args_list$rversion)
+    print("All packages -- and their dependencies -- successfully added.")
+}
+
+
 loadPackages <- function(args_list){
     print(paste0("Adding packages to repo: ", args_list$repo))
     for (i in args_list$pkgs) {
         print(paste0("Adding ", i, " ..."))
-        addPackage(args$pkgs,
-            path = args$repo,
-            repos = 'https://cloud.r-project.org/',
-            type = c("win.binary"))
+        # addPackage(args$pkgs,
+        #     path = args$repo,
+        #     repos = CRAN_mirror,
+        #     type = c("win.binary"))
     }
-
     print("All packages -- and their dependencies -- successfully added.")
 }
 
+
 checkArgs(args)
-loadPackages(args)
+if (args$create == TRUE) {
+    createRepo(args)
+} else {
+    loadPackages(args)
+}
